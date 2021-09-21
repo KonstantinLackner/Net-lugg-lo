a5157fc84accb03ce36b1d2efe42b4e7d3efc376
Konsti <lackner.konstantin@gmail.com>
2021-09-22 01:23:24 +0200

    Added trigger

---
+++ Assets/Scripts/triggerActive.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class triggerActive : MonoBehaviour
+{
+    public Sprite activated;
+    public Sprite deactivated;
+    private AudioSource source;
+    public AudioClip activateSound;
+    public AudioClip deactivateSound;
+    private bool active;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        active = false;
+        gameObject.AddComponent<AudioSource>();
+        source = gameObject.GetComponent<AudioSource>();
+    }
+
+    public void OnMouseDown()
+    {
+        if (!active)
+        {
+            active = true;
+            source.PlayOneShot(activateSound);
+            GetComponent<SpriteRenderer>().sprite = activated;
+        }
+        else
+        {
+            active = false;
+            source.PlayOneShot(deactivateSound);
+            GetComponent<SpriteRenderer>().sprite = deactivated;
+        }
+    }
+}
