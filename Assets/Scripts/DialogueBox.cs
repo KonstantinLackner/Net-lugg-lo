881236546be24ccd34d26f4998e9438f882e2030
Konsti <lackner.konstantin@gmail.com>
2021-09-27 00:53:58 +0200

    Intro dialogue

---
+++ Assets/Scripts/DialogueBox.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class DialogueBox : MonoBehaviour
+{
+    [SerializeField] private GameObject dialogueBox;
+    [SerializeField] private TMP_Text textLabel;
+    [SerializeField] private DialogueObject testDialogue;
+
+    private TypewriterEffect typewriterEffect;
+
+    private void Start()
+    {
+        typewriterEffect = GetComponent<TypewriterEffect>();
+        CloseDialogueBox();
+        ShowDialogue(testDialogue);
+    }
+
+    public void ShowDialogue(DialogueObject dialogueObject)
+    {
+        dialogueBox.SetActive(true);
+        StartCoroutine(StepThroughDialogue(dialogueObject));
+    }
+
+    private IEnumerator StepThroughDialogue(DialogueObject dialogueObject)
+    {
+        foreach (var dialogue in dialogueObject.Dialogue)
+        {
+            yield return typewriterEffect.Run(dialogue, textLabel);
+            yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.Space));
+        }
+        
+        CloseDialogueBox();
+    }
+
+    private void CloseDialogueBox()
+    {
+        dialogueBox.SetActive(false);
+        textLabel.text = string.Empty;
+    }
+}
--- Assets/Scripts/TypewriterEffect.cs
+++ Assets/Scripts/TypewriterEffect.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -5,19 +6,24 @@ using UnityEngine;
 
 public class TypewriterEffect : MonoBehaviour
 {
-    public void Run(string textToType, TMP_Text textLabel)
+
+    [SerializeField] private float typingSpeed = 50f;
+    
+    public Coroutine Run(string textToType, TMP_Text textLabel)
     {
-        StartCoroutine(TypeText(textToType, textLabel));
+        return StartCoroutine(TypeText(textToType, textLabel));
     }
 
     private IEnumerator TypeText(string textToType, TMP_Text textLabel)
     {
+        textLabel.text = String.Empty;
+        
         float t = 0;
         int charIndex = 0;
 
         while (charIndex < textToType.Length)
         {
-            t += Time.deltaTime;
+            t += Time.deltaTime * typingSpeed;
             charIndex = Mathf.FloorToInt(t);
             charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
 
